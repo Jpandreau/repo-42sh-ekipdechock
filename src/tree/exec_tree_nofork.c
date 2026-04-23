ff6fd84696825b8422b657dba4ccb3ab829ae301
Nathan genevrier <nathan.genevrier@epitech.eu>
2026-04-23 16:45:50 +0200

    [MODIF] refactor exec_tree_nofork functions to use shell_t structure for environment management

--- src/tree/exec_tree_nofork.c
+++ src/tree/exec_tree_nofork.c
@@ -7,45 +7,45 @@
 
 #include "tree.h"
 
-static int exec_logic_and_nofork(tree_t *node, char ***env)
+static int exec_logic_and_nofork(tree_t *node, shell_t *shell)
 {
-    int status = exec_tree_nofork(node->left, env);
+    int status = exec_tree_nofork(node->left, shell);
 
     if (is_exit_status(status))
         return status;
     if (status == 0)
-        return exec_tree_nofork(node->right, env);
+        return exec_tree_nofork(node->right, shell);
     return status;
 }
 
-static int exec_logic_or_nofork(tree_t *node, char ***env)
+static int exec_logic_or_nofork(tree_t *node, shell_t *shell)
 {
-    int status = exec_tree_nofork(node->left, env);
+    int status = exec_tree_nofork(node->left, shell);
 
     if (is_exit_status(status))
         return status;
     if (status != 0)
-        return exec_tree_nofork(node->right, env);
+        return exec_tree_nofork(node->right, shell);
     return status;
 }
 
-int exec_tree_nofork(tree_t *node, char ***env)
+int exec_tree_nofork(tree_t *node, shell_t *shell)
 {
     if (!node)
         return 0;
     if (prepare_tree_heredocs(node) != 0)
         return 84;
     if (node->type == TOKEN_SEQUENCE) {
-        exec_tree_nofork(node->left, env);
-        return exec_tree_nofork(node->right, env);
+        exec_tree_nofork(node->left, shell);
+        return exec_tree_nofork(node->right, shell);
     }
     if (node->type == TOKEN_AND)
-        return exec_logic_and_nofork(node, env);
+        return exec_logic_and_nofork(node, shell);
     if (node->type == TOKEN_OR)
-        return exec_logic_or_nofork(node, env);
+        return exec_logic_or_nofork(node, shell);
     if (node->type == TOKEN_PIPE)
-        return exec_pipe(node, env);
+        return exec_pipe(node, shell);
     if (node->type == TOKEN_CMD)
-        return exec_cmd_node_nofork(node, env);
+        return exec_cmd_node_nofork(node, shell);
     return 1;
 }
