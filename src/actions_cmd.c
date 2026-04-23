4ec5bd01cc923dad878c91f91cbbd4d40d28a5c2
Nathan genevrier <nathan.genevrier@epitech.eu>
2026-04-23 16:40:18 +0200

    [MODIF] refactor actions_cmd_args and actions_cmd_args_nofork to use shell_t structure for environment management

--- src/actions_cmd.c
+++ src/actions_cmd.c
@@ -8,21 +8,26 @@
 #include "my.h"
 #include "base.h"
 #include "buildin.h"
+#include "shell.h"
 
-int actions_cmd_args(char **args, char ***env)
+int actions_cmd_args(char **args, shell_t *shell)
 {
     if (args == NULL || args[0] == NULL)
         return 84;
+    if (split_buildin(args[0]))
+        return run_buildin_shell(args, shell);
     if (buildin(args[0]))
-        return run_buildin_args(args, env);
-    return exec_cmd_args(args, *env);
+        return run_buildin_args(args, &shell->env);
+    return exec_cmd_args(args, shell->env);
 }
 
-int actions_cmd_args_nofork(char **args, char ***env)
+int actions_cmd_args_nofork(char **args, shell_t *shell)
 {
     if (args == NULL || args[0] == NULL)
         return 84;
+    if (split_buildin(args[0]))
+        return run_buildin_shell(args, shell);
     if (buildin(args[0]))
-        return run_buildin_args(args, env);
-    return exec_cmd_args_nofork(args, *env);
+        return run_buildin_args(args, &shell->env);
+    return exec_cmd_args_nofork(args, shell->env);
 }
