203c96a96506c31d8b3557037dd47a4388d8aff0
mateoperrot <mateo-enzo.perrot-fieffe@epitech.eu>
2026-04-28 15:24:14 +0200

    [FIX] exec_expandargs; fix snake_case function names

---
+++ src/exec_expandargs.c
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** expand glob patterns in args before exec
+*/
+
+#include "base.h"
+#include "tokenize_extensions.h"
+#include "expandargs.h"
+
+static char **expand_one_token(char **result, char *token)
+{
+    char **expanded = NULL;
+    char **tmp = NULL;
+    int i = 0;
+
+    if (!is_glob_pattern(token))
+        return my_array_add(result, my_strdup(token));
+    if (expand_glob_pattern(token, &expanded) != 0)
+        return my_array_add(result, my_strdup(token));
+    while (expanded[i]) {
+        tmp = my_array_add(result, my_strdup(expanded[i]));
+        if (tmp)
+            result = tmp;
+        i++;
+    }
+    free_array(expanded);
+    return result;
+}
+
+char **expand_glob_args(char **args)
+{
+    char **result = malloc(sizeof(char *) * 1);
+    int i = 0;
+
+    if (!result)
+        return NULL;
+    result[0] = NULL;
+    while (args[i]) {
+        result = expand_one_token(result, args[i]);
+        if (!result)
+            return NULL;
+        i++;
+    }
+    return result;
+}
