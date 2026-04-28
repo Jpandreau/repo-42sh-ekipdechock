9e65d9339d45ed2c0e1b10750efd01b9932734b1
mateoperrot <mateo-enzo.perrot-fieffe@epitech.eu>
2026-04-28 15:24:14 +0200

    [FIX] small_headers; restore correct content and fix prototypes

--- include/small_headers.h
+++ include/small_headers.h
@@ -1,12 +1,12 @@
 /*
 ** EPITECH PROJECT, 2026
-** small_headers
+** 42sh
 ** File description:
-** small_headers
+** smallheaders
 */
 
-#ifndef SMALL_HEADERS
-    #define SMALL_HEADERS
+#ifndef SMALL_HEADERS_H
+    #define SMALL_HEADERS_H
 
     #include <stddef.h>
     #include <sys/types.h>
@@ -27,13 +27,6 @@ typedef struct history_s {
     int capacity;
 } history_t;
 
-char **tokenize_line(char *line);
-int tokenize_step(char *line, tokenize_ctx_t *ctx);
-
-int parse_exit_line(char *line, int *code);
-int exit_code_from_args(char **args, int *valid);
-int parse_exit_code_arg(char *str, int *ok);
-
 typedef struct job_state_s job_state_t;
 
 typedef struct exec_ctx_s {
@@ -41,25 +34,35 @@ typedef struct exec_ctx_s {
     job_state_t *job;
 } exec_ctx_t;
 
-int init_exec(char **line, char ***env, history_t *history, job_state_t *job);
-int handle_line(char **line, char ***env, history_t *history,
-    job_state_t *job);
-
+char **tokenize_line(char *line);
+int tokenize_step(char *line, tokenize_ctx_t *ctx);
+int parse_exit_line(char *line, int *code);
+int exit_code_from_args(char **args, int *valid);
+int parse_exit_code_arg(char *str, int *ok);
+int init_exec(char **line, char ***env,
+    history_t *history, job_state_t *job);
+int handle_line(char **line, char ***env,
+    history_t *history, job_state_t *job);
 int history_init(history_t *history);
 void history_destroy(history_t *history);
 int history_add(history_t *history, char *line);
-int history_expand_line(history_t *history, char *line, char **expanded);
-int interactive_getline(char **line, size_t *len, history_t *history);
+int history_expand_line(history_t *history,
+    char *line, char **expanded);
+int interactive_get_line(char **line,
+    size_t *len, history_t *history);
 int history_skip_spaces(char *line);
 int history_only_spaces_after(char *line, int pos);
-char *history_expand_number(char *line, int idx, history_t *history);
-char *history_expand_prefix(char *line, int idx, history_t *history);
-char *history_resolve_bang(history_t *history, char *line, int idx);
-
-int run_line(char *input_line, char ***env, int *exit_code, exec_ctx_t *ctx);
+char *history_expand_number(char *line,
+    int idx, history_t *history);
+char *history_expand_prefix(char *line,
+    int idx, history_t *history);
+char *history_resolve_bang(history_t *history,
+    char *line, int idx);
+int run_line(char *input_line, char ***env,
+    int *exit_code, exec_ctx_t *ctx);
 int open_and_stat(char *filename, struct stat *st);
 int read_content(int file, struct stat *st, char **content);
-int handle_pipe_line(char *input_line, char ***env, int *exit_code,
-    exec_ctx_t *ctx);
+int handle_pipeline(char *input_line, char ***env,
+    int *exit_code, exec_ctx_t *ctx);
 
-#endif /* SMALL_HEADERS */
+#endif
